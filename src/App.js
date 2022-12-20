608b3b8dacd9e6ccca7504b6c47c6d96e352b424
Adam Farid <afarid@stevens.edu>
2022-12-20 00:31:55 -0500

    New changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import FriendList from "./components/FriendList";
 import { friends } from "./components/Birthdays";
-import castle from "../src/images/castle-birthdays.jpg";
+import castle from "./images/castle-birthdays.jpg";
 
 function App() {
   return (
     <div>
       <div>
-        <img src={castle} alt="a" className="responsive"></img>
+        <img src={castle} alt="" className="responsive"></img>
         <h1>Castle Birthdays 🥳</h1>
         <FriendList friends={friends} />
       </div>
